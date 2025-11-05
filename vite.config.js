a947d707bbb0e37a2aeb1733d9dd5745ca41df43
Akashsagar21 <akashsagar.ind@classichome.com>
2025-11-05 16:14:03 +0530

    Update vite.config.js  updates to error fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import nodePolyfills from "vite-plugin-node-polyfills";
+import { NodeGlobalsPolyfillPlugin, NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+
 
 installGlobals({ nativeFetch: true });
 
